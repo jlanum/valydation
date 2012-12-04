999285cc7fd7aecf3aef74b5057ace7a7daebb6b
Max Lansing <lansing@gmail.com>
2012-12-04 12:52:39 -0800

    added rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HiStrollers::Application.config.secret_token = '2534e7c9c1909a01917206fb880eb7b39ac73288b24631dbfb6f43d382619f28c914879255155791773d7407f0a74e3088aae386f9f06b1a74a09faff273d17f'

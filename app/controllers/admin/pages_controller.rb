b2a0f13290b740f34328ed2e049c98a5dfb0c180
Max Lansing <lansing@gmail.com>
2013-02-12 21:54:51 -0800

    create page

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -7,6 +7,23 @@ class Admin::PagesController < ApplicationController
     @pages = Page.order("slug ASC").all
   end  
 
+  def create
+    @page = Page.new
+    @page.content = params[:page][:content]
+    @page.title = params[:page][:title]
+    @page.slug = params[:page][:slug]
+    @page.save!
+    
+    @message = "The page has been updated."
+  
+    render :template => "admin/pages/edit"
+  end
+
+  def new
+    @page = Page.new
+    render :template => "admin/pages/edit"    
+  end
+
   def edit
     @page = Page.find(params[:id])
   end

03ccf9aa30c2154ddf712a445b58241e28cb8c2e
Max Lansing <lansing@gmail.com>
2013-02-27 15:19:11 -0800

    website almost done text

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
     if @user = (User.where(:email => params[:email]).first or
                 User.where(:custom_slug => params[:email]).first)
       unless @user.activated_web
-        @error_message = "We're almost done with our web site. We will be sure to send you an email when it's ready!"
+        @error_message = "We're excited to launch our site soon. You'll be the first to know!"
       end
       unless BCrypt::Password.new(@user.passwd_hash) == params[:passwd_clear]
         @error_message = "Email/Username or password incorrect."

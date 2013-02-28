b94a3dfa3244c6f81c25363173d98e8a7c7ae41b
Max Lansing <lansing@gmail.com>
2013-02-27 21:25:00 -0800

    disable image migs

--- db/migrate/20130207180936_regenerate_web_images.rb
+++ db/migrate/20130207180936_regenerate_web_images.rb
@@ -1,24 +1,24 @@
 class RegenerateWebImages < ActiveRecord::Migration
   def up
-    Sale.all.each do |sale|
-      begin
-        puts "sale #{sale.id}"
-        if sale.image_0
-          puts "0"
-          sale.image_0.recreate_versions!
-        end
-        if sale.image_1
-          puts "1"
-          sale.image_1.recreate_versions!
-        end
-        if sale.image_2
-          puts "2"
-          sale.image_2.recreate_versions!
-        end
-      rescue Exception => e
-        puts "error #{e.to_s}"
-      end
-    end
+    #Sale.all.each do |sale|
+      #begin
+        #puts "sale #{sale.id}"
+        #if sale.image_0
+          #puts "0"
+          #sale.image_0.recreate_versions!
+        #end
+        #if sale.image_1
+          #puts "1"
+          #sale.image_1.recreate_versions!
+        #end
+        #if sale.image_2
+          #puts "2"
+          #sale.image_2.recreate_versions!
+        #end
+      #rescue Exception => e
+        #puts "error #{e.to_s}"
+      #end
+    #end
   end
 
   def down
--- db/migrate/20130211221333_recreate_sale_images_for_cwave.rb
+++ db/migrate/20130211221333_recreate_sale_images_for_cwave.rb
@@ -1,25 +1,25 @@
 class RecreateSaleImagesForCwave < ActiveRecord::Migration
   def up
-    Sale.all.each do |sale|
-      begin
-        puts "sale #{sale.id}"
-        if sale.image_0
-          puts "0"
-          sale.image_0.recreate_versions!
-        end
-        if sale.image_1
-          puts "1"
-          sale.image_1.recreate_versions!
-        end
-        if sale.image_2
-          puts "2"
-          sale.image_2.recreate_versions!
-        end
-      rescue Exception => e
-        puts "error #{e.to_s}"
-      end
-      sale.save
-    end
+    #Sale.all.each do |sale|
+      #begin
+        #puts "sale #{sale.id}"
+        #if sale.image_0
+          #puts "0"
+          #sale.image_0.recreate_versions!
+        #end
+        #if sale.image_1
+          #puts "1"
+          #sale.image_1.recreate_versions!
+        #end
+        #if sale.image_2
+          #puts "2"
+          #sale.image_2.recreate_versions!
+        #end
+      #rescue Exception => e
+        #puts "error #{e.to_s}"
+      #end
+      #sale.save
+    #end
   end
 
   def down

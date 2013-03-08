a5f0a4ca1712a204f8f7546bf413aa2733be3a74
Max Lansing <lansing@gmail.com>
2013-03-08 12:06:39 -0800

    cross-browser fixes for sale upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
       config.fog_directory  = ApplicationController.s3_bucket                     # required
       config.fog_public     = true                                   # optional, defaults to true
       config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-      config.asset_host = "http://#{config.fog_directory}.s3.amazonaws.com"
+      config.asset_host = "https://#{config.fog_directory}.s3.amazonaws.com"
     
 end
 

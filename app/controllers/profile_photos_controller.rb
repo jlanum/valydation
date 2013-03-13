2a2df0645a3a370a95e62fa55bd4903c655214f5
Max Lansing <lansing@gmail.com>
2013-03-12 20:01:34 -0700

    disable iphone redirect for upload photo controller

--- app/controllers/profile_photos_controller.rb
+++ app/controllers/profile_photos_controller.rb
@@ -1,6 +1,7 @@
 class ProfilePhotosController < ApplicationController
   before_filter :handle_device
   before_filter :require_user
+  skip_before_filter :detect_iphone
 
   def create
     @user.photo = params[:photo]

4edca644317a62d3a04757719e9e4bee71dbe18a
Jesse Lanum <jesse.lanum@gmail.com>
2013-06-02 10:48:08 -0700

    Adding Filter By Company

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -67,7 +67,6 @@ class Sale < ActiveRecord::Base
   before_save :set_brand
   before_save :set_sizes
   
-   validates_length_of :product, :minimum => 3, :maximum => 128
   
 
   def self.categories

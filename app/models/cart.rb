22456379a2c2fccaa21e01821bb724c8160545be
Max Lansing <lansing@gmail.com>
2013-06-02 16:44:49 -0700

    ensure that add-to-cart only adds one of each item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,23 +1,22 @@
 class Cart 
-attr_reader :items
-attr_accessor :current_user, :sale_id, :user_id, :item_id
-attr_accessor :image_upload_urls
- 
+  attr_reader :items
+  attr_accessor :current_user, :sale_id, :user_id, :item_id
+  attr_accessor :image_upload_urls
 
-  
+
   def initialize
-      @items = []
-    end
-
-    def add_to_cart(sale) 
-      @items << sale
-      end
-      
-      def clear
-        @items.clear
-      end
-      
-      
-      
-      
+    @items = []
   end
+
+  def add_to_cart(sale) 
+    unless @items.find { |i| i.id == sale.id }
+      @items << sale
+    end
+    @items
+  end
+
+  def clear
+    @items.clear
+  end
+
+end

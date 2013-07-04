4aa27c171ae9c841cb3c58db229eca27b1c3981c
Jesse Lanum <jesse.lanum@gmail.com>
2013-07-04 14:17:12 -0700

    Putting in Applications Controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -104,7 +104,7 @@ class PurchasesController < ApplicationController
   end
   
   def products_string
-  render_to_string(:partial => "purchases/products_email", :layout => false)
+  render_to_string(:action => "purchases/products_email", :layout => false)
   end
 
   def new_json

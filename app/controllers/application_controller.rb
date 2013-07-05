3ec20178ee324fa56a95cbfb2666e023efde0b14
Jesse Lanum <jesse.lanum@gmail.com>
2013-07-04 20:51:01 -0700

    Who Gives A Fuck AT This Point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,8 +44,9 @@ class ApplicationController < ActionController::Base
   end
   
   def products_string
-  render(:action => "purchases/products_email", :layout => false).to_s
+  products_string << render(:action => "purchases/products_email", :layout => false).to_s
   end
+  
   
   def add_to_cart
      get_cart

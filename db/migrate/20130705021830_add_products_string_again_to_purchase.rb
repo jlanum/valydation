0f1a7e338155cce08ab1e6e6229a1e37435bc686
Jesse Lanum <jesse.lanum@gmail.com>
2013-07-08 22:49:43 -0700

    Removing String In Migration

--- db/migrate/20130705021830_add_products_string_again_to_purchase.rb
+++ db/migrate/20130705021830_add_products_string_again_to_purchase.rb
@@ -1,6 +1,6 @@
 class AddProductsStringAgainToPurchase < ActiveRecord::Migration
   def change
-    remove_column :purchases, :products_email, :string
+    remove_column :purchases, :products_email
     
   end
 end
